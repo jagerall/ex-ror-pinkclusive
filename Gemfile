953648d5030f225bd4c0dc91b030833eac284720
Thai-Son-TQ <thai-son.tran-quang@mmibordeaux.com>
2023-03-01 14:43:55 +0100

    added Job Controller

--- Gemfile
+++ Gemfile
@@ -41,6 +41,7 @@ gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
 
 # Reduces boot times through caching; required in config/boot.rb
 gem "bootsnap", require: false
+gem 'bootstrap', '~> 5.2.2'
 
 # Use Sass to process CSS
 # gem "sassc-rails"
@@ -70,3 +71,5 @@ group :test do
   gem "selenium-webdriver"
   gem "webdrivers"
 end
+
+gem 'simple_form'
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_01_091441) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_01_134313) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "jobs", force: :cascade do |t|
+    t.string "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "practitioners", force: :cascade do |t|
     t.string "name"
